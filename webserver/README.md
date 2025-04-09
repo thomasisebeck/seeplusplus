0fb5ddf9b036cf44f9d08fb3f2e7d33bfe70c0ea
ThomasIsebeck <tisebeck@proton.me>
2025-04-09 18:59:13 +0200

    start the web server with some scaffholding

---
+++ webserver/README.md
@@ -0,0 +1,6 @@
+# Web Server
+
+Following the tutorial at: 
+https://www.youtube.com/watch?v=YwHErWJIh6Y
+
+
---
+++ webserver/lib/networking/Socket.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <sys/socket.h>
+
+//define a namespace to not cause name conflicts
+namespace hde {
+
+
+
+}
