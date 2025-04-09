93c4e6cbb1136abc253ebd154e87a93b72d2f147
ThomasIsebeck <tisebeck@proton.me>
2025-04-09 22:17:59 +0200

    created the socket object

--- webserver/lib/networking/Socket.cpp
+++ webserver/lib/networking/Socket.cpp
@@ -1,2 +1,11 @@
 #include "Socket.hpp"
+#include <sys/socket.h>
 
+// todo: carru on here!
+sock::Socket::Socket(Domain domain, SockType socktype) {
+
+  // int connection = socket(domain, socktype, 0);
+  // protocol chosen automatically
+  int connection =
+      socket(static_cast<int>(domain), static_cast<int>(socktype), 0);
+}
--- webserver/lib/networking/Socket.hpp
+++ webserver/lib/networking/Socket.hpp
@@ -1,24 +1,29 @@
 #pragma once
-#include <sys/socket.h>
 // include the socket library
+#include <sys/socket.h>
 
-// the type of
-enum class Domain { IPV4, IPV6, AFUNIX, AF_NS };
-
-/*
+/* The type of the domain used for the socket
  * IPV4: 32 bit ip address
  * IPV6: 128 bit ip address
  * AFUNIX: unix domain sockets (local seckets) that communicate between
  * processes on the same machine, AF_NS: Xerox networking, not widely used any
  * more
  */
+enum class Domain { IPV4, IPV6, FUNIX, AF_NS };
+
+/* The type of socket
+ * Stream: TCP under the hood, most reliable. For file transfer.
+ * Dgram: UDP under the hood, not as reliable. Less overhead. For streaming.
+ * Raw: no protocol under the hood. Need to implement all by ourselves.
+ */
+enum class SockType { SOCK_STREAM, SOCK_DGRAM, SOCK_RAW };
 
 // define a namespace to not cause name conflicts
 namespace sock {
 
 class Socket {
 public:
-  Socket(Domain domain);
+  Socket(Domain domain, SockType socktype);
 };
 
 }; // namespace sock
